265e7ea1eb226cd70d3fe4c24bbc58a562273490
Alejandro Bravo <alexbravo21@gmail.com>
2019-04-14 23:23:09 -0500

    50% del texto maquetado de la seccion interior de sabores

---
+++ sabores/sabor_int.php
@@ -0,0 +1,55 @@
+<?php include('../header.php') ?>
+
+<div class="fondo_madera_2">
+    <div class="izquierdo"></div><!--
+    --><div class="derecho">
+        <div class="sabor_interior_texto">
+            <div class="sabor_interior_titulo">
+                <p>TANG® SABOR <span>NARANJA MANGO</span></p>
+            </div>
+            <div class="porciones_cont">
+                <div class="porciones_tamanos">
+                    <div class="porciones_titulo">TAMAÑO DE PORCIÓN</div>
+                    <div class="porciones_img">
+                        <img src="<?=$site_url?>img/vaso.png" alt="" class="porciones_img_item">
+                        <div class="textbox">
+                            <p>1 vaso de 200 ml contiene</p>
+                            <p>1,5g de producto</p>
+                        </div>
+                    </div>
+                </div><!--
+                --><div class="porciones_envase">
+                    <div class="porciones_titulo">PORCIONES POR EVASE</div>
+                    <div class="porciones_img">
+                        <img src="<?=$site_url?>img/sobres/mini/naranjadamix.png" alt="" class="porciones_img_item">
+                        <div class="textbox">
+                            <p>=</p>
+                            <p>10 vasos de 200 ml</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <p class="sabor_desc_titulo">¡Prepara una deliciosa y refrescante agua sabor a limón!</p>
+            <p class="sabor_desc">
+                El agua sabor a limón de TANG® es perfecta para los días de calor. Con esta refrescante bebida dale a tus comidas un exquisito toque tropical y disfruta el delicioso sabor del Mix Naranja Mango. Con TANG®, preparar un agua sabor a limón es muy rápido y sencillo.
+            </p>
+            <div class="info_nutri_cont">
+                <p class="info_nutri_titulo">INFORMACIÓN NUTRIMENTAL</p>
+                <div class="info_nutri_tabla">
+                    <p>Contenido energético<span>20kJ / 0 kcal</span></p>
+                    <p>Proteína<span>0 g</span></p>
+                    <p>Grasas<span>0 g</span></p>
+                    <p>De las cuales: Grasa saturada<span>0 g</span></p>
+                    <p>Carbohidratos disponibles:<span>1 g</span></p>
+                    <p>De los cuales: Azúcares<span>0 g</span></p>
+                    <p>Fibra dietética<span>0 g</span></p>
+                    <p>Sodio<span>5 mg</span></p>
+                    <p>Vitamina C<span>25% IDR*</span></p>
+                </div>
+            </div>
+        </div>
+        <div class="plasta_circular"></div>
+    </div>
+</div>
+
+<?php include('../footer.php') ?>
