003bdda04064d3d735dca864eb7f940a9faf8b04
Alejandro Bravo <alexbravo21@gmail.com>
2019-04-15 18:46:55 -0500

    Terminada la sección interior de los sabores

--- sabores/sabor_int.php
+++ sabores/sabor_int.php
@@ -1,50 +1,81 @@
 <?php include('../header.php') ?>
 
 <div class="fondo_madera_2">
-    <div class="izquierdo"></div><!--
+    <div class="izquierdo">
+        <img src="<?=$site_url?>img/sobres/naranjadamix.jpg" alt="" class="sobre">
+    </div><!--
     --><div class="derecho">
-        <div class="sabor_interior_texto">
-            <div class="sabor_interior_titulo">
-                <p>TANG® SABOR <span>NARANJA MANGO</span></p>
-            </div>
-            <div class="porciones_cont">
-                <div class="porciones_tamanos">
-                    <div class="porciones_titulo">TAMAÑO DE PORCIÓN</div>
-                    <div class="porciones_img">
-                        <img src="<?=$site_url?>img/vaso.png" alt="" class="porciones_img_item">
-                        <div class="textbox">
-                            <p>1 vaso de 200 ml contiene</p>
-                            <p>1,5g de producto</p>
+        <div class="sabor_interior_cont">
+            <div class="sabor_interior_texto">
+                <div class="sabor_interior_titulo">
+                    <p>TANG® SABOR <span>NARANJA MANGO</span></p>
+                </div>
+                <div class="porciones_cont">
+                    <div class="porciones_tamanos">
+                        <div class="porciones_titulo">TAMAÑO DE PORCIÓN</div>
+                        <div class="porciones_img">
+                            <img src="<?=$site_url?>img/vaso.png" alt="" class="porciones_img_item">
+                            <div class="textbox">
+                                <p>1 vaso de 200 ml contiene</p>
+                                <p>1,5g de producto</p>
+                            </div>
                         </div>
-                    </div>
-                </div><!--
-                --><div class="porciones_envase">
-                    <div class="porciones_titulo">PORCIONES POR EVASE</div>
-                    <div class="porciones_img">
-                        <img src="<?=$site_url?>img/sobres/mini/naranjadamix.png" alt="" class="porciones_img_item">
-                        <div class="textbox">
-                            <p>=</p>
-                            <p>10 vasos de 200 ml</p>
+                    </div><!--
+                    --><div class="porciones_envase">
+                        <div class="porciones_titulo">PORCIONES POR EVASE</div>
+                        <div class="porciones_img">
+                            <img src="<?=$site_url?>img/sobres/mini/naranjadamix.png" alt="" class="porciones_img_item">
+                            <div class="textbox">
+                                <p class="porciones_envase_minitext">=</p>
+                                <p class="porciones_envase_minitext">10 vasos de 200 ml</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <p class="sabor_desc_titulo">¡Prepara una deliciosa y refrescante agua sabor a limón!</p>
-            <p class="sabor_desc">
-                El agua sabor a limón de TANG® es perfecta para los días de calor. Con esta refrescante bebida dale a tus comidas un exquisito toque tropical y disfruta el delicioso sabor del Mix Naranja Mango. Con TANG®, preparar un agua sabor a limón es muy rápido y sencillo.
-            </p>
-            <div class="info_nutri_cont">
-                <p class="info_nutri_titulo">INFORMACIÓN NUTRIMENTAL</p>
-                <div class="info_nutri_tabla">
-                    <p>Contenido energético<span class="info_nutri_num">20kJ / 0 kcal</span></p>
-                    <p>Proteína<span class="info_nutri_num">0 g</span></p>
-                    <p>Grasas<span class="info_nutri_num">0 g</span></p>
-                    <p>De las cuales: Grasa saturada<span class="info_nutri_num">0 g</span></p>
-                    <p>Carbohidratos disponibles:<span class="info_nutri_num">1 g</span></p>
-                    <p>De los cuales: Azúcares<span class="info_nutri_num">0 g</span></p>
-                    <p>Fibra dietética<span class="info_nutri_num">0 g</span></p>
-                    <p>Sodio<span class="info_nutri_num">5 mg</span></p>
-                    <p>Vitamina C<span class="info_nutri_num">25% IDR*</span></p>
+                <p class="sabor_desc_titulo">¡Prepara una deliciosa y refrescante agua sabor a limón!</p>
+                <p class="sabor_desc">
+                    El agua sabor a limón de TANG® es perfecta para los días de calor. Con esta refrescante bebida dale a tus comidas un exquisito toque tropical y disfruta el delicioso sabor del Mix Naranja Mango. Con TANG®, preparar un agua sabor a limón es muy rápido y sencillo.
+                </p>
+                <div class="info_nutri_cont">
+                    <p class="info_nutri_titulo">INFORMACIÓN NUTRIMENTAL</p>
+                    <div class="info_nutri_tabla">
+                        <p>
+                            <span class="info_nutri_texto">Contenido energético</span><!--
+                        --><span class="info_nutri_num">20kJ / 0 kcal</span>
+                        </p>
+                        <p>
+                            <span class="info_nutri_texto">Proteína</span><!--
+                        --><span class="info_nutri_num">0 g</span>
+                        </p>
+                        <p>
+                            <span class="info_nutri_texto">Grasas</span><!--
+                        --><span class="info_nutri_num">0 g</span>
+                        </p>
+                        <p>
+                            <span class="info_nutri_texto">De las cuales: Grasa saturada</span><!--
+                        --><span class="info_nutri_num">0 g</span>
+                        </p>
+                        <p>
+                            <span class="info_nutri_texto">Carbohidratos disponibles:</span><!--
+                        --><span class="info_nutri_num">1 g</span>
+                        </p>
+                        <p>
+                            <span class="info_nutri_texto">De los cuales: Azúcares</span><!--
+                        --><span class="info_nutri_num">0 g</span>
+                        </p>
+                        <p>
+                            <span class="info_nutri_texto">Fibra dietética</span><!--
+                        --><span class="info_nutri_num">0 g</span>
+                        </p>
+                        <p>
+                            <span class="info_nutri_texto">Sodio</span><!--
+                        --><span class="info_nutri_num">5 mg</span>
+                        </p>
+                        <p>
+                            <span class="info_nutri_texto">Vitamina C</span><!--
+                        --><span class="info_nutri_num">25% IDR*</span>
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
