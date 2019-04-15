5dc2343b5371ef23a0c20e7b91292873b65162b4
Alejandro Bravo <alexbravo21@gmail.com>
2019-04-14 21:38:29 -0500

    Listo el interior de recetas para todos los dispositivos

--- recetas/receta_int.php
+++ recetas/receta_int.php
@@ -3,9 +3,45 @@
 <div class="receta_interior_cont" style="background-image: url(<?=$site_url?>img/recetas/big/batido_naranja_mango.jpg);">
     <div class="izquierdo"></div><!--
 --><div class="derecho">
-    <div class="plasta_circular">
-        
-    </div>
+        <div class="receta_interior_texto">
+            <div class="receta_interior_titulo">
+                <p>BATIDO DE <span>NARANJA Y MANGO</span></p>
+            </div>
+            <div class="tiemporciones">
+                <div class="minutos">
+                    <img src="<?=$site_url?>img/reloj.png" alt="" class="reloj">
+                    <div class="textbox">
+                        <span class="minutos_num">10</span><span class="minutos_texto">MINUTOS</span>
+                    </div>
+                </div><!--
+                --><div class="porciones">
+                    <img src="<?=$site_url?>img/carita.png" alt="" class="carita">
+                    <div class="textbox">
+                        <span class="porciones_num">6</span><span class="porciones_texto">PORCIONES</span>
+                    </div>
+                </div>
+            </div>
+            <div class="receta_interior_ingredientes">
+                <p class="ingredientes_titulo">INGREDIENTES</p>
+                <ul class="ingredientes_lista">
+                    <li class="ingredientes_item">1 sobre de Tang® sabor limón</li>
+                    <li class="ingredientes_item">2 L de agua natural</li>
+                    <li class="ingredientes_item">rebanadas delgadas</li>
+                    <li class="ingredientes_item">1 pza. de limón sin semillas en rebanadas delgadas</li>
+                    <li class="ingredientes_item">1 cda. de chía</li>
+                    <li class="ingredientes_item">1 ramita de hierbabuena</li>
+                    <li class="ingredientes_item">Hielo al gusto</li>
+                </ul>
+            </div>
+            <div class="receta_interior_preparacion">
+                <p class="preparacion_titulo">PREPARACIÓN</p>
+                <ul class="preparacion_lista">
+                    <li><span>DISOLVER</span> el sobre de Tang® sabor limón en los 2 L de agua.</li>
+                    <li><span>SERVIR</span> en una garra con el pepino, limón, chía, hielo y hierbabuena.</li>
+                </ul>
+            </div>
+        </div>
+    <div class="plasta_circular"></div>
 </div>
 </div>
 
